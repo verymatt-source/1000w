13611e06b0c4492cb9f128fbed918069cbb2ebfb
verymatt-source <verymatt@gmail.com>
2025-10-28 10:37:47 +0800

    Update hs.py

--- hs.py
+++ hs.py
@@ -8,7 +8,7 @@ from operator import itemgetter # 用于列表排序操作
 import calendar # 用于辅助判断周末/交易日
 
 # --- 全局配置 ---
-OUTPUT_FILE = "index_price.html"  # 最终生成的 HTML 报告文件名
+OUTPUT_FILE = "index_price.html"  # 最终生成的 HTML 报告文件名。
 REFRESH_INTERVAL = 1800  # HTML 页面自动刷新间隔（秒），即 30 分钟
 MAX_CB_PRICE = 9999.00 # 可转债平均价计算时，剔除高于或等于此价格的标的
 
@@ -494,5 +494,6 @@ if __name__ == "__main__":
         print(f"成功更新文件: {OUTPUT_FILE}，包含 {len(all_stock_data)} 个证券/指数数据。")
     except Exception as e:
         print(f"写入文件失败: {e}")
+
 
 
